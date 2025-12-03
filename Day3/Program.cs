05ea59027a6328551d5cd48eb63e4a5f95750c6d
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2025-12-03 23:33:26 +0100

    ok

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -7,17 +7,17 @@ var size = 2; // 2 for part 1 , 12 for part 2
 var sum = 0L;
 foreach (var bank in input)
 {
-    var section = bank;
+    var bankSection = bank;
     var turnedOn = new StringBuilder(new String('0',size));
     for (var i = 0; i < size ; i++)
     {
-        var remainingBatteries = size - 1 - i;
-        var top = section.Substring(0, section.Length - remainingBatteries)
-            .Select((c,p) => (c,p))
-            .OrderByDescending(x => x.c)
-            .First();
-        turnedOn[i] = top.c;
-        section = section[(top.p + 1)..];
+        var remainingBatteries = size - i - 1;
+        var top = bankSection
+            .Take(bankSection.Length - remainingBatteries)
+            .Max();
+        turnedOn[i] = top;
+        var pos = bankSection.IndexOf(top);
+        bankSection = bankSection[(pos + 1)..];
     }
     sum += long.Parse(turnedOn.ToString());
 }
