07ec4506aea2680ae03b10e1d8f79a30e016f176
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2025-12-03 23:26:22 +0100

    ok

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,21 +1,22 @@
 ﻿using System.Text;
 
-//var fileName = "test1.txt";
-var fileName = "input.txt";
+var fileName = "test1.txt";
+//var fileName = "input.txt";
 var input = File.ReadAllLines(fileName);
-var size = 12;
+var size = 2; // 2 for part 1 , 12 for part 2
 var sum = 0L;
 foreach (var bank in input)
 {
     var section = bank;
     var turnedOn = new StringBuilder(new String('0',size));
-    for (var i = size; i >0 ; --i)
+    for (var i = 0; i < size ; i++)
     {
-        var top = section.Substring(0, section.Length - i + 1)
+        var remainingBatteries = size - 1 - i;
+        var top = section.Substring(0, section.Length - remainingBatteries)
             .Select((c,p) => (c,p))
             .OrderByDescending(x => x.c)
             .First();
-        turnedOn[size-i] = top.c;
+        turnedOn[i] = top.c;
         section = section[(top.p + 1)..];
     }
     sum += long.Parse(turnedOn.ToString());
