549fcbac13290b778c3023d33c8ffa0b57abc6c9
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2026-01-02 21:42:23 +0100

    p2 presque, mais boucle

---
+++ Day10p3/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Marche en avant pour trouver un combinaison qui fonctionne.
+ * On utilise les plus grands vecteurs d'abord
+ */
+
+var inputFile = "test1.txt";
+//inputFile = "input.txt";
+var input = File.ReadAllLines(inputFile)
+    .Select(line => line.Split(' '))
+    .Select(line => (diagrams: line[0][1..^1], wiring: line[1..^1].Select(x => x[1..^1].Split(',').Select(x => int.Parse(x)).ToArray()).ToArray(), joltage: line[^1][1..^1].Split(',').Select(x => int.Parse(x)).ToArray()))
+    .ToArray();
+
+var count = 0;
+foreach (var (_, wiring, joltage) in input)
+{
+    Console.WriteLine(string.Join(',', joltage));
+    foreach (var w in wiring)
+        Console.WriteLine(string.Join(',', w));
+    int pressed = Solve(wiring, joltage);
+    count += pressed;
+}
+
+Console.WriteLine(count);
+
+static int Solve(int[][] wiring, int[] joltage)
+{
+    wiring = wiring.OrderBy(w => w.Length).ToArray();
+
+    var stack = new Stack<int[]>();
+    stack.Push(new int[wiring.Length]);
+    while (stack.Count > 0)
+    {
+        var combinaison1 = stack.Pop();
+        var errors = joltage.Select((v, col) => v - wiring.Select((w, c) => combinaison1[c] * (w.Contains(col) ? 1 : 0)).Sum()).ToArray();
+        if (errors.Any(e => e < 0))
+            continue;
+        if (errors.All(e => e == 0))
+        {
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine(string.Join('-', combinaison1));
+            Console.WriteLine(new string('-', 30));
+            return combinaison1.Sum();
+        }
+        var blocked = false;
+        for (var i = 0; i < errors.Length; i++)
+        {
+            var error = errors[i];
+            if (error != 0)
+                continue;
+            for (var j = 0; j < wiring.Length; j++)
+            {
+                if (!wiring[j].Contains(i))
+                    continue;
+
+            }
+        }
+        if (blocked)
+            continue;
+        var count = 0;
+        foreach (var w in wiring)
+        {
+            var combinaison = combinaison1.ToArray();
+            combinaison[count++]++;
+            stack.Push(combinaison);
+        }
+    }
+    throw new Exception("Not found");
+}
+
--- day10p2/Program.cs
+++ day10p2/Program.cs
@@ -3,8 +3,10 @@
  * On utilise les plus grands vecteurs d'abord
  */
 
+using System.Diagnostics;
+
 var inputFile = "test1.txt";
-//inputFile = "input.txt";
+inputFile = "input.txt";
 var input = File.ReadAllLines(inputFile)
     .Select(line => line.Split(' '))
     .Select(line => (diagrams: line[0][1..^1], wiring: line[1..^1].Select(x => x[1..^1].Split(',').Select(x => int.Parse(x)).ToArray()).ToArray(), joltage: line[^1][1..^1].Split(',').Select(x => int.Parse(x)).ToArray()))
@@ -13,11 +15,12 @@ var input = File.ReadAllLines(inputFile)
 var count = 0;
 foreach (var (_, wiring, joltage) in input)
 {
+    var watch = Stopwatch.StartNew();
     Console.WriteLine(string.Join(',', joltage));
-    foreach (var w in wiring)
-        Console.WriteLine(string.Join(',', w));
     int pressed = Solve(wiring, joltage);
     count += pressed;
+    watch.Stop();
+    Console.WriteLine($"Elapsed {watch.ElapsedMilliseconds}ms");
 }
 
 Console.WriteLine(count);
@@ -25,28 +28,60 @@ Console.WriteLine(count);
 static int Solve(int[][] wiring, int[] joltage)
 {
     wiring = wiring.OrderBy(w => w.Length).ToArray();
+    foreach (var w in wiring)
+        Console.WriteLine(string.Join(',', w));
 
     var stack = new Stack<int[]>();
     stack.Push(new int[wiring.Length]);
+    var distance = int.MaxValue;
     while (stack.Count > 0)
     {
-        var combinaison1 = stack.Pop();
-        var errors = joltage.Select((v, col) => v - wiring.Select((w, c) => combinaison1[c] * (w.Contains(col) ? 1 : 0)).Sum()).ToArray();
-        if (errors.Any(e => e < 0))
-            continue;
+        var combi = stack.Pop();
+        var errors = joltage.Select((v, col) => v - wiring.Select((w, c) => combi[c] * (w.Contains(col) ? 1 : 0)).Sum()).ToArray();
         if (errors.All(e => e == 0))
         {
             Console.WriteLine(new string('-', 30));
-            Console.WriteLine(string.Join('-', combinaison1));
+            Console.WriteLine(string.Join('-', combi));
             Console.WriteLine(new string('-', 30));
-            return combinaison1.Sum();
+            return combi.Sum();
         }
-        var i = 0;
-        foreach (var w in wiring)
+        var currentDist = errors.Sum();
+        if (currentDist < distance)
         {
-            var combinaison = combinaison1.ToArray();
-            combinaison[i++]++;
+            distance = currentDist;
+            Console.WriteLine($"distance = {distance}");
+        }
+        var indexMin = errors.Select((e, i) => (e, i)).Where(r => r.e > 0).OrderBy(r => r.e).Select(r => r.i).First();
+        var indexMax = errors.Select((e, i) => (e, i)).OrderByDescending(r => r.e).Select(r => r.i).First();
+        for (var index = 0; index < wiring.Length;index++)
+        {
+            var w = wiring[index]; 
+            if (errors.Where((e,i) => e == 0 && w.Contains(i)).Any())
+                continue;
+            if (indexMin != indexMax && (w.Contains(indexMin) ) )
+                continue;
+            if (indexMin != indexMax && (! w.Contains(indexMax)))
+                continue;
+            var combinaison = combi.ToArray();
+            combinaison[index++]++;
             stack.Push(combinaison);
+            //if (errors.Any(e => e == 0))
+            //{
+            //    var indexNull = errors
+            //        .Select((valeur, index) => new { valeur, index })
+            //        .Where(x => x.valeur == 0)
+            //        .Select(x => x.index)
+            //        .ToList();
+            //    var indexNotNull = errors
+            //        .Select((valeur, index) => new { valeur, index })
+            //        .Where(x => x.valeur > 0)
+            //        .Select(x => x.index)
+            //        .ToList();
+            //    if (indexNotNull
+            //        .Any(i => wiring.Where(w => w.Contains(i))
+            //        .All(w => w.Any(j => indexNull.Contains(j)))))
+            //        continue;
+            //}
         }
     }
     throw new Exception("Not found");
